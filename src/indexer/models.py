d4bca252399ea1bdf39836f011a8e7469b763846
xShahadx <shahad.m.ibrahem@gmail.com>
2026-01-16 02:23:45 +0300

    enh: delete cascade

--- src/indexer/models.py
+++ src/indexer/models.py
@@ -19,8 +19,8 @@ class Repository(Base):
     default_branch: Mapped[str] = mapped_column(String(30))
     avatar_url: Mapped[str] = mapped_column(nullable=True)
 
-    files: Mapped[List["File"]] = relationship(back_populates="repository")
-    topics: Mapped[List["RepositoryTopic"]] = relationship(back_populates="repository")
+    files: Mapped[List["File"]] = relationship(back_populates="repository", cascade="all, delete-orphan")
+    topics: Mapped[List["RepositoryTopic"]] = relationship(back_populates="repository", cascade="all, delete-orphan")
 
     def __repr__(self):
         return f"Repository(id={self.id!r}, owner={self.owner!r}, name={self.name!r}, description={self.description!r}, url={self.url!r}, forks_count={self.forks_count!r}, open_issues_count={self.open_issues_count!r}, default_branch={self.default_branch!r}, avatar_url={self.avatar_url!r})"
@@ -38,7 +38,7 @@ class File(Base):
     file_path: Mapped[str]
     content_hash: Mapped[str] = mapped_column(nullable=True)
 
-    chunks: Mapped[List["Chunk"]] = relationship(back_populates="file")
+    chunks: Mapped[List["Chunk"]] = relationship(back_populates="file", cascade="all, delete-orphan")
 
     def __repr__(self):
         return f"File(id={self.id!r}, repository_id={self.repository_id!r}, commit_sha={self.commit_sha!r}, file_path={self.file_path!r}, content_hash={self.content_hash!r})"
@@ -60,7 +60,7 @@ class Chunk(Base):
 
     chunk_parent_id: Mapped[int] = mapped_column(ForeignKey("chunk.id"), nullable=True)
     chunk_parent: Mapped["Chunk"] = relationship(back_populates="children_chunks", remote_side=[id])
-    children_chunks: Mapped[List["Chunk"]] = relationship(back_populates="chunk_parent")
+    children_chunks: Mapped[List["Chunk"]] = relationship(back_populates="chunk_parent", cascade="all, delete-orphan")
 
     start_line: Mapped[int] 
     end_line: Mapped[int] 
